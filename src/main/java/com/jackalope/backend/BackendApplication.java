2a373a8ac2ba120825d7e0b1ad7343c5480c0467
corey <csmaller@elumatherapy.com>
2025-11-03 13:29:46 -0700

    cors

--- src/main/java/com/jackalope/backend/BackendApplication.java
+++ src/main/java/com/jackalope/backend/BackendApplication.java
@@ -3,19 +3,19 @@ package com.jackalope.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
+//import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
-		System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
-		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
+		// Dotenv dotenv = Dotenv.load();
+		// System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+		// System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+		// System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+		// System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		// System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		// System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
 
 		SpringApplication.run(BackendApplication.class, args);
 	}

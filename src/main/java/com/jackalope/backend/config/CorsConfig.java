15ba2e3ff069fe57d2f7758ceec0b0adcdf72d67
corey <csmaller@elumatherapy.com>
2025-11-14 15:16:36 -0700

    added jackalope to cors

--- src/main/java/com/jackalope/backend/config/CorsConfig.java
+++ src/main/java/com/jackalope/backend/config/CorsConfig.java
@@ -10,9 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "https://jackalope-blog.netlify.app")
+                .allowedOrigins("http://localhost:4200", "https://jackalope-blog.netlify.app", "https://www.jackalopeadventures.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
